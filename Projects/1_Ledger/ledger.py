5b700b96cd9b98ef4ed92252d3815b2804d082a5
rathore287 <harshraj.rathore@gmail.com>
2019-12-14 13:22:37 +0000

    major major fixes

---
+++ Projects/1_Ledger/ledger.py
@@ -0,0 +1,16 @@
+import json
+
+ledger_json = {"Python" : {"23/11/2019" : "who knows"}}
+
+with open("text", "w") as file:
+    json.dump({'numbers':n, 'strings':s, 'x':x, 'y':y}, file, indent=4))
+with open("text") as file:
+    result = file.read()
+file.close()
+print (type(result))
+print (result.keys())
+print (result)
+python
+class ledger(self, topics):
+pass
+ledger_json
---
+++ Python/Tkinter/Python-Is-Fun-master/README.md
@@ -0,0 +1,1 @@
+# Python Program for downloading Youtube Videos
